ecf2426b70fd6f8f0d55b294054b84f551cc9713
Kumud Bhandari <kumud.bhandari@hpe.com>
2016-05-24 20:34:04 -0500

    introduced the hblkhdr and some macros

---
+++ makalu_alloc/src/internal_includes/makalu_config.h
@@ -0,0 +1,43 @@
+#ifndef _MAKALU_CONFIG_H
+#define _MAKALU_CONFIG_H
+
+# include "atomic_ops.h"
+
+
+
+//typedefs
+typedef unsigned long long word;
+typedef long long signed_word;
+typedef AO_t counter_t;
+
+
+//block
+#define HBLKSIZE 4096
+#define define CPP_LOG_HBLKSIZE 12
+
+
+//granule
+#define CPP_WORDSZ 64
+#define GRANULE_BYTES 16
+
+#define MARK_BITS_PER_HBLK (HBLKSIZE/GRANULE_BYTES) 
+  /* upper bound */
+
+//struct hblkhdr hb_flags possible values
+
+# define IGNORE_OFF_PAGE  1 /* Ignore pointers that do not  */
+                            /* point to the first page of   */
+                            /* this object.                 */
+# define FREE_BLK 4 /* Block is free, i.e. not in use.      */
+
+
+//struct hblkhdr page_reclaim_state possible values
+#define IN_RECLAIMLIST 0
+#define IN_FLOATING 1
+
+
+//struct hblkhdr hb_marks
+#define MARK_BITS_SZ (MARK_BITS_PER_HBLK/CPP_WORDSZ + 1)
+
+
+#endif
